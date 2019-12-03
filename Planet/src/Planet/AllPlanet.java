772d7eb8c1ad2562e26a1614310241f3bf4c27db
vuend <57929455+vuend@users.noreply.github.com>
2019-12-03 21:31:13 +0800

    Add files via upload

---
+++ Planet/src/Planet/AllPlanet.java
@@ -0,0 +1,23 @@
+package Planet;
+
+public class AllPlanet {
+    public static void main(String[] args) {
+        Planet Mercury = new Planet();
+        Mercury.displaySize(2440);
+        Planet Venus = new Planet();
+        Venus.displaySize(6052);
+        Planet Earth = new Planet();
+        Earth.displaySize(6371);
+        Planet Mars = new Planet();
+        Mars.displaySize(3390);
+        Planet Jupiter = new Planet();
+        Jupiter.displaySize(69911);
+        Planet Saturn = new Planet();
+        Saturn.displaySize(58232);
+        Planet Uranus = new Planet();
+        Uranus.displaySize(25362);
+        Planet Neptune = new Planet();
+        Neptune.displaySize(24622);
+        }
+    }
+}
---
+++ Planet/src/Planet/Planet.java
@@ -0,0 +1,11 @@
+package Planet;
+//////////Introduction to Objects/////////////
+public class Planet {
+
+    int size;
+
+    public void displaySize(int size) {
+        System.out.println("The size of this plant is " + size);
+
+    }
+}
